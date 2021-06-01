957e7c50c7b7aed9176959bce556e21dfbce6b87
Tibi <tiberiutiprigan112@gmail.com>
2021-06-01 16:16:46 +0300

    fix pentru baza de date, crearea unei noi coloane

--- resources/database.sql
+++ resources/database.sql
@@ -1,9 +1,10 @@
 CREATE TABLE `users` (
-  `usersId` int(11) NOT NULL,
+  `usersId` int(11) NOT NULL AUTO_INCREMENT,
   `usersName` varchar(128) NOT NULL,
   `usersEmail` varchar(128) NOT NULL,
   `usersUid` varchar(128) NOT NULL,
-  `usersPwd` varchar(128) NOT NULL
+  `usersPwd` varchar(128) NOT NULL,
+   PRIMARY KEY (`usersId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `colab` (
@@ -12,13 +13,13 @@ CREATE TABLE `colab` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `cod` (
-  `codId` int(11) NOT NULL,
+  `codId` int(11) NOT NULL AUTO_INCREMENT,
   `codUsersName` varchar(128) NOT NULL,
   `codName` varchar(128) NOT NULL,
   `codValability` int(11) NOT NULL,
   `codVisibility` varchar(128) NOT NULL,
   `codPwd` varchar(128) DEFAULT NULL,
   `codeText` longtext NOT NULL,
-  `creation_date` date NOT NULL DEFAULT current_timestamp()
+  `creation_date` date NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`codId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
